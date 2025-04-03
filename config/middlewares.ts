6e8cc732c5c6bebb3fcbb0295a31c09a37387be2
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2025-04-03 23:04:00 +0100

    fix middlewares

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,22 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [
+        "https://blog-react-strapi-lovat.vercel.app", // Allow Vercel frontend
+        "http://localhost:3000", // Allow local development
+      ],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
